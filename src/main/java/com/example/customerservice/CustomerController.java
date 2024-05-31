a879b9ab55813de452d21c38d442f615659b97de
Samuel <xxsamuelsanchezx@gmail.com>
2024-05-31 08:55:35 -0500

    add cors and another customer

--- src/main/java/com/example/customerservice/CustomerController.java
+++ src/main/java/com/example/customerservice/CustomerController.java
@@ -2,15 +2,16 @@ package com.example.customerservice;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class CustomerController {
 
-    @GetMapping("/customer")
+    @GetMapping("/customers")
     public ResponseEntity<?> getCustomer(@RequestParam String documentType, @RequestParam String documentNumber) {
         if (!documentType.equals("C") && !documentType.equals("P")) {
             return new ResponseEntity<>("Tipo de documento no válido", HttpStatus.BAD_REQUEST);
@@ -27,6 +28,18 @@ public class CustomerController {
             customer.setPhone("555-1234");
             customer.setAddress("Calle 123");
             customer.setCity("Bogotá");
+
+            return new ResponseEntity<>(customer, HttpStatus.OK);
+        }
+        if (documentType.equals("C") && documentNumber.equals("11111111")) {
+            Customer customer = new Customer();
+            customer.setFirstName("Jhenny");
+            customer.setSecondName("Juliana");
+            customer.setFirstSurname("Pérez");
+            customer.setSecondSurname("Aponte");
+            customer.setPhone("444-55112");
+            customer.setAddress("Av siempre viva");
+            customer.setCity("Cali");
 
             return new ResponseEntity<>(customer, HttpStatus.OK);
         }
